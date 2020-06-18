89d86b9e3ff27f32faf7aef0b1b2fad551ddb68e
Anshuman <anshumank726@gmail.com>
2020-06-18 20:21:00 +0530

    Add solution for problem `A - C+=` Link: https://codeforces.com/contest/1368/problem/A Tags: brute force, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1368/submission/84174956

---
+++ codeforces/1368/A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+const int MAX = 100001;
+const int mod = 1e9 + 7;
+vector<int> g[MAX];
+int par[MAX];
+bool vis[MAX];
+
+bool cmp(pair<int, int> p, pair<int, int> q) {
+	return p.second < q.second;
+}
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	cin >> t;
+	while (t--) {
+		int a, b, n;
+		cin >> a >> b >> n;
+		int ans = 0;
+		while (!(a > n || b > n)) {
+			int x = min(a, b);
+			int y = max(a, b);
+			a = x + y;
+			b = y;
+			ans++;
+		}
+		cout << ans << endl;
+	}
+}
