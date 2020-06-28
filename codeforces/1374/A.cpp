5d9953f669a997e43c012850ac7a3bb8535dcd5d
Anshuman <anshumank726@gmail.com>
2020-06-28 20:10:00 +0530

    Add solution for problem `A - Required Remainder` Link: https://codeforces.com/contest/1374/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1374/submission/85299978

---
+++ codeforces/1374/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+const int MAX = 100001;
+const int mod = 1e9 + 7;
+vector<int> g[MAX];
+int par[MAX];
+bool vis[MAX];
+
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t; cin >> t;
+	while (t--) {
+		int x, y, n;
+		cin >> x >> y >> n;
+		int m = n % x;
+		int ans = n + y - m;
+		if (ans > n)
+			ans -= x;
+		cout << ans  << endl;
+	}
+}
