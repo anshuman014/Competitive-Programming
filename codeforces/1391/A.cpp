9e1bacddd6549129d84f0e02db0f5746acd959f1
Anshuman <anshumank726@gmail.com>
2020-08-09 20:15:00 +0530

    Add solution for problem `A - Suborrays` Link: https://codeforces.com/contest/1391/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1391/submission/89418040

---
+++ codeforces/1391/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+#define f first
+#define s second
+#define mp make_pair
+
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int MAX = 100001;
+const int mod = 1e9 + 7;
+vector<int> g[MAX];
+int par[MAX];
+bool vis[MAX];
+
+int n, x;
+ll dp[1001][1001];
+
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+	//t = 1;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; i++)
+			cout << i << " ";
+		cout << endl;
+	}
+
+}
