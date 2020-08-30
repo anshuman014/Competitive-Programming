75ca062d3de659c3a99474e960a708658c88aac6
Anshuman <anshumank726@gmail.com>
2020-08-30 20:09:00 +0530

    Add solution for problem `A - Juggling Letters` Link: https://codeforces.com/contest/1397/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1397/submission/91353509

---
+++ codeforces/1397/A.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if ((int)name.size()) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+
+typedef long long ll;
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+#define f first
+#define s second
+#define mp make_pair
+
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int MAX = 200001;
+const int mod = 1e9 + 7;
+vector<int> g[MAX];
+int par[MAX];
+bool vis[MAX];
+ll dp[1001][1001];
+
+int n;
+int maxd = 0, node = 0;
+
+void dfs(int x, int d) {
+	vis[x] = 1;
+	if (d > maxd) {
+		maxd = d;
+		node = x;
+	}
+	for (int u : g[x]) {
+		if (!vis[u]) {
+			dfs(u, d + 1);
+		}
+	}
+}
+
+const int m = 1e6 ;
+int f[m + 1];
+
+int get(int n) {
+	int ans = 0;
+	for (int i = n; i < 1e6 + 1; i += n) {
+		ans += f[i];
+	}
+	return ans;
+}
+
+int main() {
+
+	setIO();
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		string s[n];
+		int f[26] = {0};
+		for0(i, n) {
+			cin >> s[i];
+			for (char c : s[i]) {
+				f[c - 'a']++;
+			}
+		}
+		bool yes = true;
+		for0(i, 26) {
+			if (f[i] % n) {
+				yes = false;
+				break;
+			}
+		}
+		if (yes)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+
+}
+
