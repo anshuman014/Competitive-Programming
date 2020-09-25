f0ba1d6505ee04e1cbe734f2b898b7d99f34167c
Anshuman <anshumank726@gmail.com>
2020-09-25 12:56:00 +0530

    Add solution for problem `A - New Bus Route` Link: https://codeforces.com/contest/792/problem/A Tags: implementation, sortings, *1100 Ref: https://codeforces.com/contest/792/submission/93760349

---
+++ codeforces/792/A.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if ((int)name.size()) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+
+typedef long long ll;
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+#define f first
+#define s second
+#define mp make_pair
+
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int MAX = 200001;
+const int mod = 1e9 + 7;
+vector<int> g[MAX];
+int par[MAX];
+int dist[MAX];
+bool vis[MAX];
+int rank[MAX];
+
+
+int main() {
+
+	setIO();
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int n;
+	cin >> n;
+	std::vector<int> a(n);
+	for0(i, n) cin >> a[i];
+	int mx = INT_MAX;
+	sort(all(a));
+	int cnt = 0;
+	for (int i = 1; i < n; i++) {
+		if (a[i] - a[i - 1] < mx) {
+			cnt = 1;
+			mx = a[i] - a[i - 1];
+		}
+		else if (a[i] - a[i - 1] == mx) {
+			cnt++;
+		}
+	}
+	cout << mx << " " << cnt;
+
+}
