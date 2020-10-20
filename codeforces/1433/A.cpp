fec91d68679f251ecd5fb5b556cfc7e8076cbbc0
Anshuman <anshumank726@gmail.com>
2020-10-20 20:09:00 +0530

    Add solution for problem `A - Boring Apartments` Link: https://codeforces.com/contest/1433/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1433/submission/96084084

---
+++ codeforces/1433/A.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	// if ((int)name.size()) {
+	// 	freopen((name + ".in").c_str(), "r", stdin);
+	// 	freopen((name + ".out").c_str(), "w", stdout);
+	// }
+}
+
+
+typedef long long ll;
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+#define f first
+#define s second
+#define mp make_pair
+
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int MAX = 200001;
+const int mod = 1e9 + 7;
+const ll modulo = 998244353;
+vector<vector<int>> g;
+vector<int> vis;
+vector<int> par;
+vector<int> dist;
+
+void initgraph(int n) {
+	g.resize(n + 1);
+	vis.resize(n + 1);
+	par.resize(n + 1);
+	dist.resize(n + 1);
+}
+
+
+void solve() {
+	int n;
+	cin >> n;
+	int digits = 0;
+	int m = n;
+	while (m > 0) {
+		digits++;
+		m /= 10;
+	}
+
+	int x = n % 10;
+	int ans = (x - 1) * 10;
+	if (digits == 1)
+		ans += 1;
+	else if (digits == 2)
+		ans += 3;
+	else if (digits == 3)
+		ans += 6;
+	else
+		ans += 10;
+	cout << ans << endl;
+}
+
+
+
+int main() {
+
+	setIO();
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int T = 1;
+	cin >> T;
+	while (T--) {
+		solve();
+	}
+}
+
+
+
