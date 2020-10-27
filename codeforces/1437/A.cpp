e5fe6c7a696dd4b039106fcda7ebfb4a4cac9308
Anshuman <anshumank726@gmail.com>
2020-10-27 20:10:00 +0530

    Add solution for problem `A - Marketing Scheme` Link: https://codeforces.com/contest/1437/problem/A Tags: brute force, constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1437/submission/96869132

---
+++ codeforces/1437/A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	// if ((int)name.size()) {
+	// 	freopen((name + ".in").c_str(), "r", stdin);
+	// 	freopen((name + ".out").c_str(), "w", stdout);
+	// }
+}
+
+
+typedef long long ll;
+#define int long long
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+#define f first
+#define s second
+#define mp make_pair
+
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int MAX = 100001;
+const int mod = 1e9 + 7;
+const ll modulo = 998244353;
+const int inf = 1e9;
+const ll infinity = 1e18;
+vector < vector<pair<int, int>> >g;
+vector<int> vis;
+vector<int> par;
+vector<ll> dist;
+
+int n, m;
+
+void initgraph(int n) {
+	g.resize(n + 1);
+	vis.resize(n + 1);
+	par.resize(n + 1);
+	dist.resize(n + 1);
+}
+
+void solve() {
+	int a, b;
+	cin >> a >> b;
+	if (b < 2 * a)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+}
+
+
+
+int32_t main() {
+
+	setIO();
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int T = 1;
+	cin >> T;
+	while (T--) {
+		solve();
+	}
+}
