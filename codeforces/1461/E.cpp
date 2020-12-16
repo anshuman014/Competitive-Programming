3093e50c5717fdf2842c28dd870a536827b976e7
Anshuman <anshumank726@gmail.com>
2020-12-16 16:33:00 +0530

    Add solution for problem `E - Water Level` Link: https://codeforces.com/contest/1461/problem/E Tags: brute force, graphs, greedy, implementation, math, *2200 Ref: https://codeforces.com/contest/1461/submission/101416503

---
+++ codeforces/1461/E.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if ((int)name.size()) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+
+typedef long long ll;
+#define int long long
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+#define f first
+//#define s second
+#define mp make_pair
+
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int MAX = 200005;
+const int mod = 1e9 + 7;
+const ll modulo = 998244353;
+vector<int> g[MAX];
+vector<int> par(MAX);
+int dist[MAX];
+bool vis[MAX];
+vector<int> ran(MAX);
+
+int parent(int x) {
+	if (par[x] == x)
+		return x;
+	return par[x] = parent(par[x]);
+}
+
+void sunion(int a, int b) {
+	int x = parent(a), y = parent(b);
+	if (x == y)
+		return;
+	if (ran[x] < ran[y])
+		swap(x, y);
+	par[y] = x;
+	ran[x] += ran[y];
+}
+
+set<int> st;
+
+/*void recurse(int start, int last, vector<int> &a, vector<int> &pre) {
+	if (start >= last)
+		return;
+	int small = a[start], big = a[last];
+	int poss = pre[last] - pre[start];
+	s.insert(poss);
+	int mid = (small + big) / 2;
+	int split = upper_bound(all(a), mid) - a.begin();
+	recurse(start, split - 1, a, pre);
+	recurse(split, last, a, pre);
+}*/
+
+
+void recurse(int s, int e, vector<int> &a) {
+	if (s > e)
+		return;
+	int sum = 0;
+	for (int i = s; i <= e; i++)
+		sum += a[i];
+	int pos = -1;
+	st.insert(sum);
+	int mid = (a[s] + a[e]) / 2;
+	for (int i = s; i <= e; i++) {
+		if (a[i] <= mid)
+			pos = i;
+		else
+			break;
+	}
+	if (pos == -1 || pos == e)
+		return;
+	recurse(s, pos, a);
+	recurse(pos + 1, e, a);
+}
+
+bool solve() {
+	int k, l, r, t, x, y;
+	cin >> k >> l >> r >> t >> x >> y;
+	if (k + y <= r)
+		k += y;
+	if (k < l || k > r) {
+		return false;
+	}
+
+	if (y < x) {
+		k -= x;
+		int take = (k - l) / (x - y) + 1;
+		return t <= take;
+	}
+
+	for (int i = 0; i < x; i++) {
+		int take = (k - l) / x;
+		k -= take * x;
+		if (t <= take)
+			return true;
+		t -= take;
+
+		if (take == 0)
+			return false;
+
+
+		k += y;
+		if (k > r)
+			return false;
+	}
+	return true;
+
+}
+
+
+
+int32_t main() {
+
+	setIO();
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	//cin >> t;
+	while (t--)
+	{
+		if (solve())
+			cout << "Yes";
+		else
+			cout << "No";
+	}
+	return 0;
+}
