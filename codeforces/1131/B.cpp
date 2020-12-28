db23cff254fa8be15ff78357b539f95a3cdde8b8
Anshuman <anshumank726@gmail.com>
2020-12-28 13:44:00 +0530

    Add solution for problem `B - Draw!` Link: https://codeforces.com/contest/1131/problem/B Tags: greedy, implementation, *1400 Ref: https://codeforces.com/contest/1131/submission/102521999

---
+++ codeforces/1131/B.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIO(string name = "") {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	if ((int)name.size()) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+
+typedef long long ll;
+#define int long long
+#define pb push_back
+#define endl "\n"
+#define for0(i,n) for(int i=0;i<n;i++)
+#define for1(i,n) for(int i=1;i<n;i++)
+#define ps(x,y) fixed << setprecision(y) << x
+
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+using pi = pair<int, int>;
+//#define f first
+//#define s second
+//#define mp make_pair
+
+int dx[] = { -1, 0, 1, 0};
+int dy[] = {0, 1, 0, -1};
+
+const int MAX = 200005;
+const int mod = 1e9 + 7;
+const ll modulo = 998244353;
+const int inf = 1e9 + 5;
+const ll infinity = 1e18;
+
+const int N = 2e5 + 5;
+vector<int> g[N];
+int vis[N];
+int groups[N];
+pair<int, int> e[N];
+
+
+
+void solve() {
+	int n;
+	cin >> n;
+	int ans = 0;
+	int pa = 0, pb = 0;
+	while (n--) {
+		int a, b;
+		cin >> a >> b;
+	/*	if (a == pa && b == pb)
+			continue;*/
+		int take = min(a, b) - max(pa, pb) + 1;
+		ans += take > 0 ? take : 0;
+
+		pa = a;
+		pb = b;
+		if (pa == pb)
+			pa++, pb++;
+	}
+	cout << ans;
+
+}
+
+
+
+
+int32_t main() {
+
+	setIO();
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t = 1;
+	//cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
+	return 0;
+}
+
